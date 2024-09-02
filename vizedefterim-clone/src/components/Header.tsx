3abcbdfaccedc16eb7c3429077243fe5cfa31648
benadidafouzia <benadidafouzia@gmail.com>
2024-09-02 14:46:57 +0300

    refactor

--- vizedefterim-clone/src/components/Header.tsx
+++ vizedefterim-clone/src/components/Header.tsx
@@ -1,4 +1,3 @@
-// src/components/Header/Header.tsx
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaMoon, FaSun, FaCog, FaTimes, FaUndo } from "react-icons/fa";
